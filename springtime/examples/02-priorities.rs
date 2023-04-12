edc4d69913154d764ab692b4e1cf6a777f87c886
Kamil Rojewski <kamil.rojewski@gmail.com>
2023-04-12 17:21:26 +0200

    priority example

---
+++ springtime/examples/02-priorities.rs
@@ -0,0 +1,74 @@
+// note: this example assumes you've analyzed the previous one
+
+use springtime::application;
+use springtime::runner::{ApplicationRunner, BoxFuture};
+use springtime_di::future::FutureExt;
+use springtime_di::instance_provider::ErrorPtr;
+use springtime_di::{component_alias, Component};
+
+#[derive(Component)]
+struct PrintHelloRunner;
+
+#[component_alias]
+impl ApplicationRunner for PrintHelloRunner {
+    fn run(&self) -> BoxFuture<'_, Result<(), ErrorPtr>> {
+        async {
+            print!("Hello ");
+            Ok(())
+        }
+        .boxed()
+    }
+
+    // for ordered execution of application runners, priorities can be used
+    fn priority(&self) -> i8 {
+        3
+    }
+}
+
+#[derive(Component)]
+struct PrintWorldRunner;
+
+#[component_alias]
+impl ApplicationRunner for PrintWorldRunner {
+    fn run(&self) -> BoxFuture<'_, Result<(), ErrorPtr>> {
+        async {
+            print!("world");
+            Ok(())
+        }
+        .boxed()
+    }
+
+    // for ordered execution of application runners, priorities can be used
+    fn priority(&self) -> i8 {
+        2
+    }
+}
+
+#[derive(Component)]
+struct PrintExclamationRunner;
+
+#[component_alias]
+impl ApplicationRunner for PrintExclamationRunner {
+    fn run(&self) -> BoxFuture<'_, Result<(), ErrorPtr>> {
+        async {
+            println!("!");
+            Ok(())
+        }
+        .boxed()
+    }
+
+    // for ordered execution of application runners, priorities can be used
+    fn priority(&self) -> i8 {
+        1
+    }
+}
+
+// note: for the sake of simplicity, errors are unwrapped, rather than gracefully handled
+#[tokio::main]
+async fn main() {
+    let mut application =
+        application::create_default().expect("unable to create default application");
+
+    // prints "Hello world!"
+    application.run().await.expect("error running application");
+}
