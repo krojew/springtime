be15def45b1faf2c3f661383e88aae9cad1ff3fb
Kamil Rojewski <kamil.rojewski@gmail.com>
2023-04-12 09:55:33 +0200

    basic usage example

---
+++ springtime/examples/00-basic-app.rs
@@ -0,0 +1,33 @@
+use springtime::application;
+use springtime::runner::{ApplicationRunner, BoxFuture};
+use springtime_di::future::FutureExt;
+use springtime_di::instance_provider::ErrorPtr;
+use springtime_di::{component_alias, Component};
+
+// this is an application runner, which will run when the application starts; the framework will
+// automatically discover it using dependency injection
+#[derive(Component)]
+struct HelloWorldRunner;
+
+#[component_alias]
+impl ApplicationRunner for HelloWorldRunner {
+    // note: BoxFuture is only needed when using the "async" feature
+    fn run(&self) -> BoxFuture<'_, Result<(), ErrorPtr>> {
+        async {
+            println!("Hello world!");
+            Ok(())
+        }
+        .boxed()
+    }
+}
+
+// note: for the sake of simplicity, errors are unwrapped, rather than gracefully handled
+#[tokio::main]
+async fn main() {
+    // create our application, which will detect all runners
+    let mut application =
+        application::create_default().expect("unable to create default application");
+
+    // prints "Hello world!"
+    application.run().await.expect("error running application");
+}
