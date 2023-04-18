f4a982571b9aa890128c24954c4258d6c33f85c6
Kamil Rojewski <kamil.rojewski@gmail.com>
2023-04-18 09:41:47 +0200

    configuring routers

--- springtime-web-axum/src/controller.rs
+++ springtime-web-axum/src/controller.rs
@@ -1,5 +1,9 @@
 //! Functionality related to defining [Controller]s.
 
+use axum::Router;
+use fxhash::FxHashSet;
+#[cfg(test)]
+use mockall::automock;
 use springtime_di::injectable;
 
 /// Main trait for [Components](springtime_di::component::Component) used as controllers -
@@ -7,10 +11,19 @@ use springtime_di::injectable;
 /// structs. Such approach allows for injecting other components via dependency injection, and
 /// therefore, creating advanced applications with proper architecture.
 #[injectable]
+#[cfg_attr(test, automock)]
 pub trait Controller {
     /// Prefix for all paths contained in the controller, e.g. controller path of `/abc` and handler
     /// path of `/xyz` results in final path of `/abc/xyz`.
     fn path(&self) -> Option<String> {
         None
     }
+
+    /// Optional list of server names for which given controller should be registered.
+    fn server_names(&self) -> Option<FxHashSet<String>> {
+        None
+    }
+
+    /// Configures given [Router] to handle incoming requests.
+    fn configure_router(&self, router: Router) -> Router;
 }
--- springtime-web-axum/src/router.rs
+++ springtime-web-axum/src/router.rs
@@ -1,9 +1,10 @@
 //! Controller routing handling. By default, routing is based on gathering existing controllers and
 //! their request handlers.
 
+use crate::controller::Controller;
 use axum::Router;
 use springtime_di::component_registry::conditional::unregistered_component;
-use springtime_di::instance_provider::ErrorPtr;
+use springtime_di::instance_provider::{ComponentInstancePtr, ErrorPtr};
 use springtime_di::{component_alias, injectable, Component};
 
 /// Trait for creating a [Router], usually based on injected
@@ -16,11 +17,51 @@ pub trait RouterBootstrap {
 
 #[derive(Component)]
 #[component(priority = -128, condition = "unregistered_component::<dyn RouterBootstrap + Send + Sync>")]
-struct ControllerRouterBootstrap;
+struct ControllerRouterBootstrap {
+    controllers: Vec<ComponentInstancePtr<dyn Controller + Send + Sync>>,
+}
 
 #[component_alias]
 impl RouterBootstrap for ControllerRouterBootstrap {
-    fn bootstrap_router(&self, _server_name: &str) -> Result<Router, ErrorPtr> {
-        todo!()
+    fn bootstrap_router(&self, server_name: &str) -> Result<Router, ErrorPtr> {
+        Ok(self
+            .controllers
+            .iter()
+            .filter(|controller| {
+                controller
+                    .server_names()
+                    .map(|server_names| server_names.contains(server_name))
+                    .unwrap_or(true)
+            })
+            .fold(Router::new(), |router, controller| {
+                controller.configure_router(router)
+            }))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::controller::MockController;
+    use crate::router::{ControllerRouterBootstrap, RouterBootstrap};
+    use fxhash::FxHashSet;
+    use springtime_di::instance_provider::ComponentInstancePtr;
+
+    #[test]
+    fn should_configure_router_with_filtering() {
+        let mut controller = MockController::new();
+        controller
+            .expect_configure_router()
+            .times(1)
+            .returning(|router| router);
+        controller.expect_server_names().times(1).return_const(
+            ["1".to_string(), "2".to_string()]
+                .into_iter()
+                .collect::<FxHashSet<_>>(),
+        );
+
+        let bootstrap = ControllerRouterBootstrap {
+            controllers: vec![ComponentInstancePtr::new(controller)],
+        };
+        assert!(bootstrap.bootstrap_router("1").is_ok());
     }
 }
